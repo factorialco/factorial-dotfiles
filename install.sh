caef5d0c3b50ac2ba62f5c5371c26f2b3cecbd64
Pau Ramon Revilla <masylum@gmail.com>
2022-05-30 16:50:05 +0200

    Added install.sh

---
+++ install.sh
@@ -0,0 +1,10 @@
+sh -c "$(curl -fsLS chezmoi.io/get)"
+chezmoi init
+# TODO: add to path?
+cd ~/.local/share/chezmoi
+git remote add origin https://github.com/masylum/factorial-dotfiles.git
+# TODO: Move repo to factorial
+git branch -M main
+git pull origin main
+~/bin/chezmoi apply
+source ~/.bashrc
