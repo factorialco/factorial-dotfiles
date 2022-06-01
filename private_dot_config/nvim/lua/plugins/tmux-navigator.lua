f0ae9b6fee44f119f604ddfee709f2657c6fd8c1
Victor Blasco <victor.bf@gmail.com>
2022-06-01 22:33:44 +0200

    Replace 'M' with 'C' for tmux-navigator prefix

--- private_dot_config/nvim/lua/plugins/tmux-navigator.lua
+++ private_dot_config/nvim/lua/plugins/tmux-navigator.lua
@@ -1,7 +1,7 @@
 vim.cmd("let g:tmux_navigator_disable_when_zoomed = 1")
 vim.cmd("let g:tmux_navigator_no_mappings = 1")
 
-vim.keymap.set("n", "<M-h>", ":TmuxNavigateLeft<CR>", { silent = true })
-vim.keymap.set("n", "<M-j>", ":TmuxNavigateDown<CR>", { silent = true })
-vim.keymap.set("n", "<M-k>", ":TmuxNavigateUp<CR>", { silent = true })
-vim.keymap.set("n", "<M-l>", ":TmuxNavigateRight<CR>", { silent = true })
+vim.keymap.set("n", "<C-h>", ":TmuxNavigateLeft<CR>", { silent = true })
+vim.keymap.set("n", "<C-j>", ":TmuxNavigateDown<CR>", { silent = true })
+vim.keymap.set("n", "<C-k>", ":TmuxNavigateUp<CR>", { silent = true })
+vim.keymap.set("n", "<C-l>", ":TmuxNavigateRight<CR>", { silent = true })
