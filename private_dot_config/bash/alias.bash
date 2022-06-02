e3a556b6af3a2030ce4f6e8d7abba7e68d1b0e40
Victor Blasco <victor.bf@gmail.com>
2022-06-02 13:53:45 +0200

    Update git aliases

--- private_dot_config/bash/alias.bash
+++ private_dot_config/bash/alias.bash
@@ -16,12 +16,18 @@ alias jc='cd ~/.local/share/chezmoi/'
 
 # GIT Aliases
 alias gap='git add -p'
-alias gs='git status'
+alias gst='git status'
 alias gl='git pull'
 alias gpu='git push'
 alias gc='git commit'
 alias gcm='git commit -m'
 alias gco='git checkout'
-alias gg='git grep'
-alias gst='git diff master --compact-summary'
+# alias gg='git grep'
+# alias gst='git diff master --compact-summary'
 alias gca='git commit --amend'
+alias gda='git diff --cached'
+alias gbr='git branch'
+alias gfp='git fetch -p'
+alias gpnew='!git push -u origin $(git rev-parse --abbrev-ref HEAD)'
+alias gwip="!git add --all . && git commit -m 'WIP'"
+alias gl="git log --pretty=format:'%Cred%h%Creset - %C(green) %an %C(reset) - %C(yellow)%d%Creset %s %Cgreen(%cr) %Creset'"
