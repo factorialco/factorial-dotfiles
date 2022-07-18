a3e88766aeccee39a93bddd2614002d0b79a8ef7
Victor Blasco <victor.bf@gmail.com>
2022-07-18 12:33:54 +0200

    Revert "Merge branch 'main' into main"
    This reverts commit 7d8d9426b6bf589f2103956929dd9b6177701cc6, reversing
    changes made to 5993a8f965d9ed1102aeb1326a061210bf474d3d.

--- install.sh
+++ install.sh
@@ -16,18 +16,3 @@ source ~/.bashrc
 nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync' -u ~/.config/nvim/lua/plugins.lua
 
 # TODO: Add the same for treesitter
-
-# Git aliases & config
-git config --global push.default tracking
-git config --global pull.default tracking
-git config --global alias.co 'checkout'
-git config --global alias.br 'branch'
-git config --global alias.ci 'commit'
-git config --global alias.st 'status'
-git config --global alias.sl 'stash list'
-git config --global alias.sp 'stash pop'
-git config --global alias.l "log --pretty=format:'%Cred%h%Creset - %C(green) %an %C(reset) - %C(yellow)%d%Creset %s %Cgreen(%cr) %Creset'"
-git config --global alias.da 'diff --cached'
-git config --global alias.pushnew "!git push -u origin $(git rev-parse --abbrev-ref HEAD)"
-git config --global alias.wip "!git add --all . && git commit -m 'WIP'"
-git config --global alias.fp 'fetch -p'
--- private_dot_config/bash/alias.bash
+++ private_dot_config/bash/alias.bash
@@ -9,25 +9,16 @@ then
   alias tree="find . -print | sed -e 's;[^/]*/;|____;g;s;____|; |;g'"
 fi
 
-# Jumps
-alias jb='cd /workspace/backend/'
-alias jf='cd /workspace/frontend/'
-alias jc='cd ~/.local/share/chezmoi/'
+# GIT
 
-# GIT Aliases
+# Aliases
 alias gap='git add -p'
-alias gst='git status'
+alias gs='git status'
 alias gl='git pull'
 alias gpu='git push'
 alias gc='git commit'
 alias gcm='git commit -m'
 alias gco='git checkout'
-# alias gg='git grep'
-# alias gst='git diff master --compact-summary'
+alias gg='git grep'
+alias gst='git diff master --compact-summary'
 alias gca='git commit --amend'
-alias gda='git diff --cached'
-alias gbr='git branch'
-alias gfp='git fetch -p'
-alias gpnew='!git push -u origin $(git rev-parse --abbrev-ref HEAD)'
-alias gwip="!git add --all . && git commit -m 'WIP'"
-alias gl="git log --pretty=format:'%Cred%h%Creset - %C(green) %an %C(reset) - %C(yellow)%d%Creset %s %Cgreen(%cr) %Creset'"
